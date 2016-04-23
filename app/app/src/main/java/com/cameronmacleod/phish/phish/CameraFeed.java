db96d856e76d70d63cea57212b4a135411430c8f
Cameron MacLeod <cmacleod170@googlemail.com>
2016-04-23 17:09:45 +0100

    Add in camera preview
    Permissions don't yet work to production level, but well enough for demo

--- app/app/src/main/java/com/cameronmacleod/phish/phish/CameraFeed.java
+++ app/app/src/main/java/com/cameronmacleod/phish/phish/CameraFeed.java
@@ -1,13 +1,65 @@
 package com.cameronmacleod.phish.phish;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 public class CameraFeed extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private int CAMERA_PERMISSION = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_feed);
+
+        if (checkCameraHardware() && checkCameraPermission()) {
+            mCamera = getCameraInstance();
+            Log.d("Main activity", mCamera.toString());
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+        else {
+            Log.e("Main activity", "No camera available");
+        }
     }
+
+    private boolean checkCameraHardware() {
+        return this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    private boolean checkCameraPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CALENDAR);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                CAMERA_PERMISSION);
+        return true;
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(0);
+        }
+        catch (Exception e){
+            Log.e("Camera exception", "{} - camera failed".format(e.getMessage()));
+        }
+        return c;
+    }
+
+
 }
