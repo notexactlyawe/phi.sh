b53951eb740fc80099a832f2efd73c642a07e902
Cameron MacLeod <cmacleod170@googlemail.com>
2016-04-24 01:49:15 +0100

    Change camerafeed to use camera2
    This required a rewrite of CameraFeed and activity_camera_feed
    CameraPreview got removed
    We have an ImageReader that should be doing the sending of images
    The sending is not yet implemented

--- app/app/src/main/java/com/cameronmacleod/phish/phish/CameraFeed.java
+++ app/app/src/main/java/com/cameronmacleod/phish/phish/CameraFeed.java
@@ -1,64 +1,247 @@
 package com.cameronmacleod.phish.phish;
 
+import java.security.Permission;
+import java.util.Arrays;
+
 import android.Manifest;
-import android.content.Context;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
-import android.widget.FrameLayout;
+import android.util.Size;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class CameraFeed extends AppCompatActivity {
+public class CameraFeed extends Activity {
 
-    private Camera mCamera;
-    private CameraPreview mPreview;
-    private int CAMERA_PERMISSION = 42;
+    private final static String TAG = "SimpleCamera";
+    private TextureView mTextureView = null;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListner =
+            new TextureView.SurfaceTextureListener() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_feed);
-
-        if (checkCameraHardware() && checkCameraPermission()) {
-            mCamera = getCameraInstance();
-            Log.d("Main activity", mCamera.toString());
-            mPreview = new CameraPreview(this, mCamera);
-            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-            preview.addView(mPreview);
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            //Log.i(TAG, "onSurfaceTextureUpdated()");
         }
-        else {
-            Log.e("Main activity", "No camera available");
-        }
-    }
 
-    private boolean checkCameraHardware() {
-        return this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
-    }
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width,
+                                                int height) {
+            Log.i(TAG, "onSurfaceTextureSizeChanged()");
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            Log.i(TAG, "onSurfaceTextureDestroyed()");
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            Log.i(TAG, "onSurfaceTextureAvailable()");
+
+            CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+            try{
+                String cameraId = manager.getCameraIdList()[0];
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(
+                        CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+
+                if(checkCameraPermission()) {
+                    manager.openCamera(cameraId, mStateCallback, null);
+                }
+            }
+            catch(CameraAccessException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    };
 
     private boolean checkCameraPermission() {
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_CALENDAR);
-        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED)
+        {
             return true;
         }
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.CAMERA},
-                CAMERA_PERMISSION);
+        else
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    42);
+        }
         return true;
     }
 
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(0);
+    private Size mPreviewSize = null;
+    private CameraDevice mCameraDevice = null;
+    private CaptureRequest.Builder mPreviewBuilder = null;
+    private CameraCaptureSession mPreviewSession = null;
+    private ImageReader imageReader = null;
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            Log.i(TAG, "onOpened");
+            mCameraDevice = camera;
+
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            if (texture == null) {
+                Log.e(TAG, "texture is null");
+                return;
+            }
+
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface surface = new Surface(texture);
+
+            HandlerThread backgroundThread = new HandlerThread("CameraSend");
+            backgroundThread.start();
+            Handler backgroundHandler = new Handler(backgroundThread.getLooper());
+
+            imageReader = ImageReader.newInstance(mPreviewSize.getWidth(),
+                    mPreviewSize.getHeight(), ImageFormat.YUV_420_888, 2);
+            imageReader.setOnImageAvailableListener(onImAvListen, backgroundHandler);
+
+            try {
+                mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            } catch (CameraAccessException e){
+                e.printStackTrace();
+            }
+
+            mPreviewBuilder.addTarget(surface);
+            mPreviewBuilder.addTarget(imageReader.getSurface());
+
+            try {
+                mCameraDevice.createCaptureSession(Arrays.asList(surface, imageReader.getSurface()),
+                        mPreviewStateCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
         }
-        catch (Exception e){
-            Log.e("Camera exception", "{} - camera failed".format(e.getMessage()));
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            Log.e(TAG, "onError");
         }
-        return c;
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            Log.e(TAG, "onDisconnected");
+        }
+    };
+
+    private final ImageReader.OnImageAvailableListener onImAvListen =
+            new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = reader.acquireLatestImage();
+                    if(image == null) {
+                        return;
+                    }
+                    // From here we will send it to server
+                    Log.d("Image available", String.format("Size: {} {}",
+                            image.getWidth(), image.getHeight()));
+                    image.close();
+                }
+            };
+
+    private CameraCaptureSession.StateCallback mPreviewStateCallback =
+            new CameraCaptureSession.StateCallback() {
+
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            Log.i(TAG, "onConfigured");
+            mPreviewSession = session;
+
+            mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            HandlerThread backgroundThread = new HandlerThread("CameraPreview");
+            backgroundThread.start();
+            Handler backgroundHandler = new Handler(backgroundThread.getLooper());
+
+            try {
+                mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(),
+                        null, backgroundHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+            Log.e(TAG, "CameraCaptureSession Configure failed");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //same as set-up android:screenOrientation="portrait" in <activity>, AndroidManifest.xml
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_camera_feed);
+
+        Button hello = (Button) findViewById(R.id.stopRecButton);
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mPreviewSession != null) {
+                    try {
+                        mPreviewSession.stopRepeating();
+                    }
+                    catch(CameraAccessException e) {
+                        Log.e("Oh shit", "We had a cameraaccessexception");
+                    }
+                }
+            }
+        });
+
+        mTextureView = (TextureView) findViewById(R.id.textureView1);
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListner);
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+
+        if (mCameraDevice != null)
+        {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
     }
 
 
